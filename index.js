e7778503b0b2fd138d9b226bb0033395c5c24079
William <michalkaliszuk7@gmail.com>
2024-12-17 15:03:15 +0100

    Added email function
    Awaiting tests

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
 
-function createWindow() {
-    const mainWindow = new BrowserWindow({
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
             nodeIntegration: true,
         },
     });
-
+    
     mainWindow.loadFile('index.html');
-}
-
-app.whenReady().then(createWindow);
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
-
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
 });
