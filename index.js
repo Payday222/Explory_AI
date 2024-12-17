b3b97d6af4576166ee550a7c86633743df944b3b
William <michalkaliszuk7@gmail.com>
2024-12-17 14:35:37 +0100

    Added login, signup, email and password validity checks

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
-let mainWindow;
-
-app.on('ready', () => {
-    mainWindow = new BrowserWindow({
+function createWindow() {
+    const mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
@@ -12,4 +11,18 @@ app.on('ready', () => {
     });
 
     mainWindow.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
 });
