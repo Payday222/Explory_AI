096a6153b95661a7393ded4a375f9f746a29453c
William <michalkaliszuk7@gmail.com>
2024-12-21 18:54:44 +0100

    Sql integration complete

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 dotenv.config({ path: './db.env'});
