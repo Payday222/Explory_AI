618e1327edde7295de92ac330d33369bbe159dcc
William <michalkaliszuk7@gmail.com>
2025-02-03 21:57:39 +0100

    Revert "almost finished with dark/lightmode switch"
    This reverts commit b0b40571c42a1b6b8808df45a160e57ee869bd93.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const { app, BrowserWindow, ipcMain, session } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require('electron');
 
 let mainWindow;
 
@@ -8,35 +7,12 @@ app.on('ready', () => {
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js'), 
-            nodeIntegration: false, // false for security
-            contextIsolation: true, 
-            webSecurity: false,  // This is still needed for other features but won't fix cookie issue directly
+            nodeIntegration: true,
         },
         fullscreen: true,
     });
-
-    mainWindow.loadURL('http://localhost:8080');  // Serving via local server
-
+    
+    mainWindow.loadFile('index.html');
     mainWindow.webContents.openDevTools();
 
-    // Handle cookie setting via IPC
-    ipcMain.handle('set-cookie', async (event, name, value) => {
-        await session.defaultSession.cookies.set({
-            url: 'http://localhost:8080',  // Use the correct URL
-            name: name,
-            value: value,
-            expirationDate: Math.floor(Date.now() / 1000) + (30 * 24 * 60 * 60) // 30 days
-        });
-        console.log(`Cookie set: ${name}=${value}`);
-    });
-
-    // Handle cookie getting via IPC
-    ipcMain.handle('get-cookie', async (event, name) => {
-        const cookies = await session.defaultSession.cookies.get({
-            url: 'http://localhost:8080'  // Use the correct URL here too
-        });
-        const cookie = cookies.find(cookie => cookie.name === name);
-        return cookie ? cookie.value : null;
-    });
 });
