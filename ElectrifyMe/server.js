7735aef46aa28f1ab54fc3e05e6b11a26e2a0bc9
William <michalkaliszuk7@gmail.com>
2025-02-11 15:13:18 +0100

    Latest stable checkpoint with electrifyme

--- ElectrifyMe/server.js
+++ ElectrifyMe/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000
 const app = express();
 
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'Public')));
 
 
 const expressServer = app.listen(PORT, () => console.log(`listen on port ${PORT}`))
