bec42f1a088d7f9da6b9cd8fa1cc6e72ce775228
William <michalkaliszuk7@gmail.com>
2025-02-13 03:05:47 +0100

    ipc

--- preload.js
+++ preload.js
@@ -3,4 +3,6 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("electron", {
     getCookie: (name) => ipcRenderer.invoke("get-cookie", name),
     setCookie: (name, value) => ipcRenderer.invoke("set-cookie", name, value),
+    sendData: (data) => ipcRenderer.send('send-data', data),
+    onDataReceived: (callback) => ipcRenderer.on('receive-data', (_, data) => callback(data))
 });
