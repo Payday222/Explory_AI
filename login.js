2f4d3650953fd429ca2569b6254d935fc45adc3e
William <michalkaliszuk7@gmail.com>
2025-02-13 06:28:54 +0100

    data

--- login.js
+++ login.js
@@ -43,7 +43,6 @@ loginButton.addEventListener('click', () => {
             if (data.success) {
                 console.log("Login successful!");
                 window.open('http://188.127.1.110:3005');
-                window.open('http://188.127.1.110:3005');
             } else {
                 console.log("Invalid credentials.");
             }
