6d40486a794cd6020702f9cb91110af91ae5c09d
William <michalkaliszuk7@gmail.com>
2025-02-13 05:09:36 +0100

    Revert "merge of ansdev n master"
    This reverts commit 2356807402bab5a74e9fd325d1aec3ee6dac80f2, reversing
    changes made to dd2c2c9acc366fea351836f2057ec2434405564f.

--- login.js
+++ login.js
@@ -42,15 +42,8 @@ loginButton.addEventListener('click', () => {
         .then(data => {
             if (data.success) {
                 console.log("Login successful!");
-                console.log(window.electron);
-                if(window.electron) {
-                    window.electron.openWindow('http://188.127.1.110:3005');
-                    window.electron.openWindow('http://188.127.1.110:3005');
-
-                } else {
-                    console.log('window not electron');
-                    
-                }
+                window.open('http://188.127.1.110:3005');
+                window.open('http://188.127.1.110:3005');
             } else {
                 console.log("Invalid credentials.");
             }
--- preload.js
+++ preload.js
@@ -3,7 +3,4 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("electron", {
     getCookie: (name) => ipcRenderer.invoke("get-cookie", name),
     setCookie: (name, value) => ipcRenderer.invoke("set-cookie", name, value),
-    openWindow: (url) => ipcRenderer.send("open-window", url),
-    sendDataToMain: (data) => ipcRenderer.send('send-data', data),  // Send data to main process
-    receiveDataFromMain: (callback) => ipcRenderer.on('receive-data', (event, data) => callback(data))  // Receive data in host
 });
