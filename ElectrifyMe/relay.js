d024ff36fc97c5809656418a6ac31f8bf50e87af
William <michalkaliszuk7@gmail.com>
2025-02-21 13:01:05 +0100

    final touches, hopefully

--- ElectrifyMe/relay.js
+++ ElectrifyMe/relay.js
@@ -1,8 +1,8 @@
 // server.js
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io'); // Import the Server class from socket.io
-const { io: Client } = require('socket.io-client'); // Import the client from socket.io-client
+const { Server } = require('socket.io');
+const { io: Client } = require('socket.io-client');
 
 const app = express();
 const server = http.createServer(app);
@@ -13,23 +13,22 @@ const io = new Server(server, {
         allowedHeaders: ["my-custom-header"],
         credentials: true
     }
-}); // Create a new instance of the Server class with CORS options
+});
 const botSocket = Client('http://188.127.1.110:3007'); // Connect to the bot server
 
 app.use(express.static('public')); // Serve static files from the 'public' directory
 
-// Handle socket connections
+
 io.on('connection', (socket) => {
     console.log("[Server] A client connected");
 
-    // Handle joining a room
+    
     socket.on('joinRoom', (roomCode) => {
         socket.join(roomCode);
         console.log(`[Server] Client joined room: ${roomCode}`);
         socket.to(roomCode).emit('joinedRoom', roomCode);
     });
 
-    // Handle sending answers
     socket.on('sendAnswers', (data) => {
         const { roomCode, answers, clientID  } = data;
         console.log(`[Server] Received answers for room ${roomCode}: ${answers}`);
@@ -39,19 +38,19 @@ io.on('connection', (socket) => {
         botSocket.emit('evaluateAnswer', {...data}); // Forward to the bot for evaluation
     });
 
-    // Handle disconnection
+    
     socket.on('disconnect', () => {
         console.log("[Server] A client disconnected");
     });
 
     botSocket.on('EvaluationResponse', (data) => {
-        const { response } = data;
-        console.log('Received EvaluationResponse from bot:', response);
-        // Send the evaluation result only to the original sender
+        const { response, clientID } = data;
+        console.log('Received EvaluationResponse from bot:', response, 'for clientID:', clientID);
+        io.to(clientID).emit('EvaluationResponse', { response });
     });
+    
 });
 
-// Start the server
 const PORT = process.env.PORT || 3008;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
