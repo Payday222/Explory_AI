abaf43b60401ed66f6d59db9107a47abfd821535
William <michalkaliszuk7@gmail.com>
2025-02-21 12:46:05 +0100

    fixd typo

--- ElectrifyMe/relay.js
+++ ElectrifyMe/relay.js
@@ -31,7 +31,7 @@ io.on('connection', (socket) => {
 
     // Handle sending answers
     socket.on('sendAnswers', (data) => {
-        const { roomCode, answers, clientID } = data;
+        const { roomCode, answers, clientID  } = data;
         console.log(`[Server] Received answers for room ${roomCode}: ${answers}`);
         console.log("clientID recieved: ", clientID);
         // Broadcast the answers to all clients in the room
